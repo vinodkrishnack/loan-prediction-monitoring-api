ce802365d26aded4284cfead8d4d2d2de17f0359
Vinod Krishna <vinodkrishnack@gmail.com>
2025-08-25 17:01:08 -0700

    Enable CORS

--- app/main.py
+++ app/main.py
@@ -4,20 +4,16 @@ import joblib
 import numpy as np
 import os
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You can restrict this to your frontend domain in production
+    allow_origins=["*"],  # Or use ["https://ml-frontend-91qc.vercel.app"] for more security
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.get("/")
 async def prediction_root():
